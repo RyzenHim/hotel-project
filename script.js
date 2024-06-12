996b2e7684674289b6b960e2f7e9ae98b290ec15
Himanshu Amulya Upadhyay <90633968+RyzenHim@users.noreply.github.com>
2024-06-12 11:20:36 +0530

    jss added

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+function hideNav() {
+  let hn = document.querySelector(".mobile_list");
 
+  hn.style.display = "none";
+}
+function showNav() {
+  let sn = document.querySelector(".mobile_list");
+  sn.style.display = "inline";
+}
+
+let hn = document.querySelectorAll("#navsvg");
+hn.style.cursor = "pointer";
